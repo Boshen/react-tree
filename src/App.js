1704a41db003ee6651974ddda94da9c8bff03e38
Boshen Chen <boshenc@gmail.com>
2017-06-04 09:49:50 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Tree } from './Tree'
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <h1>React Tree</h1>
+        <Tree />
+      </main>
+    )
+  }
+}
+
+export default App
